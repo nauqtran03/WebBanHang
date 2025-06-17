2c7d0cb03a15b35827d460048ff55e8a4dcd5572
nauqtran03 <quantranhk3@gmail.com>
2025-06-17 21:47:28 +0700

    addtocart

---
+++ WebBanHang/WebBanHang/Models/ShoppingCart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHang.Models
+{
+    public class ShoppingCart
+    {
+        public List<ShoppingCartItem> item { get; set; }; 
+        public ShoppingCart() 
+        {
+            this.item = new List<ShoppingCartItem>();
+        }
+        public void AddToCart(int? id, int? quantity)
+        {
+
+        }
+    }
+    public class ShoppingCartItem
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductImg {  get; set; }
+        public int Quantity {  get; set; }
+        public decimal Price { get; set; }
+        public string TotalPrice { get; set; }
+    }
+}
---
+++ WebBanHang/WebBanHang/Scripts/jsShopping.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('body').on('click', '.btnAddToCart', function (e) {
+        e.preventDefault();
+        var id = $(this).data('id');
+        var quantity = 1;
+        var total_quantity = $('#quantity_value').text();
+        if (total_quantity != '') {
+            quantity = parseInt(total_quantity)
+        }
+        alert(id + " " + quantity);
+    });
+});
