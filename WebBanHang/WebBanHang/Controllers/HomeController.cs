ba75401629df1a3e347860662f7c90eafce01d33
nauqtran03 <quantranhk3@gmail.com>
2025-06-21 10:34:15 +0700

    viewaccess

--- WebBanHang/WebBanHang/Controllers/HomeController.cs
+++ WebBanHang/WebBanHang/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBanHang.Models;
 
 namespace WebBanHang.Controllers
 {
@@ -19,7 +20,21 @@ namespace WebBanHang.Controllers
 
             return View();
         }
+        public ActionResult Refresh()
+        {
+            var item = new ReportModel();
 
+            ViewBag.Visitors_online = HttpContext.Application["visitors_online"];
+            var hn = HttpContext.Application["HomNay"];
+            item.HomNay = HttpContext.Application["HomNay"].ToString();
+            item.HomQua = HttpContext.Application["HomQua"].ToString();
+            item.TuanNay = HttpContext.Application["TuanNay"].ToString();
+            item.TuanTruoc = HttpContext.Application["TuanTruoc"].ToString();
+            item.ThangNay = HttpContext.Application["ThangNay"].ToString();
+            item.ThangTruoc = HttpContext.Application["ThangTruoc"].ToString();
+            item.TatCa = HttpContext.Application["TatCa"].ToString();
+            return PartialView(item);
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
---
+++ WebBanHang/WebBanHang/Models/Common/AnalyticsAccess.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Data;
+namespace WebBanHang.Models.Common
+{
+    public class AnalyticsAccess
+    {
+        private static string strConnect = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+
+        public static ReportViewModel Report()
+        {
+            using (var connect = new SqlConnection(strConnect))
+            {
+                var item = connect.QueryFirstOrDefault<ReportViewModel>("sp_Reports",commandType: CommandType.StoredProcedure);
+                return null;
+            }
+        }
+    }
+}
