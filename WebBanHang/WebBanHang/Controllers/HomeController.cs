1499fe9d8ca75d730793f966495af02b83773e42
nauqtran03 <quantranhk3@gmail.com>
2025-06-27 22:22:38 +0700

    login&register

--- WebBanHang/WebBanHang/Controllers/HomeController.cs
+++ WebBanHang/WebBanHang/Controllers/HomeController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebBanHang.Models;
+using WebBanHang.Models.EF;
 
 namespace WebBanHang.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbConnect = new ApplicationDbContext();
         public ActionResult Index()
         {
             return View();
@@ -34,6 +36,22 @@ namespace WebBanHang.Controllers
             item.ThangTruoc = HttpContext.Application["ThangTruoc"].ToString();
             item.TatCa = HttpContext.Application["TatCa"].ToString();
             return PartialView(item);
+        }
+        public ActionResult Partial_Subscribe()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Subscribe(Subscribe req)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbConnect.Subscribes.Add(new Subscribe { Email = req.Email, CreatedDate = DateTime.Now });
+                _dbConnect.SaveChanges();
+                return Json(new {success=true});
+            }
+            return View("Parital_Subscribe",req);
         }
         public ActionResult Contact()
         {
